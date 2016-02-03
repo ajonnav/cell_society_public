793066ece9ad44b05e03dc33a5872d6bc9a97415
Colin Duffy <theduffman96@gmail.com>
2016-02-02 21:42:04 -0500

    Moved the Cell Classes to a Cell Package

---
+++ src/simulations/CA.java
@@ -0,0 +1,13 @@
+package simulations;
+import java.util.*;
+import cells.*;
+public abstract class CA {
+	private int cellWidth;
+	private int cellHeight;
+	private int[][] neighbors;
+	private static final int[] STATE_CODES;
+	private boolean simOver;
+	private Collection<Cells> allCells;
+	
+	
+}
