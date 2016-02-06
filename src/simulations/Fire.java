2bb2037904e1e2f7be90a4424c4c40f87b9b42e8
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-05 20:58:50 -0500

    Edited to allow cells to be set as burning or empty in XML.

--- src/simulations/Fire.java
+++ src/simulations/Fire.java
@@ -6,18 +6,24 @@ import java.util.Map;
 
 import FireCells.FireBURNING;
 import FireCells.FireCell;
+import FireCells.FireEMPTY;
 import FireCells.FireTREE;
 import automaton.AutomatonDisplay;
 
 public class Fire extends CA {
 	private double probCatch;
 	private FireCell[] allCells;
+	private ArrayList<Double> burning_x;
+	private ArrayList<Double> burning_y;
+	private ArrayList<Double> empty_x;
+	private ArrayList<Double> empty_y;
 	
 	public Fire (Map<String, String> mapArgs, AutomatonDisplay a) {
 		super(mapArgs, a);
 		// TODO Auto-generated constructor stub
 		probCatch = Double.parseDouble(mapArgs.get("probCatch"));
 		allCells = new FireCell[getNumCell()];
+		getListsfromXMLMap(mapArgs);
 	}
 
 	@Override
@@ -26,10 +32,13 @@ public class Fire extends CA {
 		int numCell = 0;
 		for (int i = 0; i < getNumRow(); i++) {
 			for (int j = 0; j < getNumCol(); j++) {
-				if (Math.random() < 0.1) {
+				if (burning_x.contains(i) && burning_y.contains(j)) {
 					getAllCells()[numCell] = new FireBURNING(i*getCellWidth(), j*getCellHeight(), getCellWidth(), getCellHeight());
-				}
-				else {
+				} else if (empty_x.contains(i) && empty_y.contains(j)) {
+					getAllCells()[numCell] = new FireEMPTY(i*getCellWidth(), j*getCellHeight(), getCellWidth(), getCellHeight());
+				} else if (Math.random() < 0.1) { //this should not need to be here
+					getAllCells()[numCell] = new FireBURNING(i*getCellWidth(), j*getCellHeight(), getCellWidth(), getCellHeight());
+				} else {
 					getAllCells()[numCell] = new FireTREE(i*getCellWidth(), j*getCellHeight(), getCellWidth(), getCellHeight(), probCatch);
 				}
 				numCell++;
@@ -87,6 +96,25 @@ public class Fire extends CA {
 	
 	private void setAllCells(FireCell[] newStates) {
 		allCells = Arrays.copyOf(newStates, newStates.length);
+	}
+	
+	private void getListsfromXMLMap(Map<String, String> mapArgs) {
+		String [] burn_x = mapArgs.get("BurningCells_x").split("\\s+");
+		String [] burn_y = mapArgs.get("BurningCells_y").split("\\s+");
+		String [] emp_x = mapArgs.get("Empty_x").split("\\s+");
+		String [] emp_y = mapArgs.get("Empty_y").split("\\s+");
+		burning_x = new ArrayList<Double>();
+		burning_y = new ArrayList<Double>();
+		empty_x = new ArrayList<Double>();
+		empty_y = new ArrayList<Double>();
+		for (int i = 0; i < burn_x.length; i++) {
+			burning_x.add(Double.parseDouble(burn_x[i]));
+			burning_y.add(Double.parseDouble(burn_y[i]));
+		}
+		for (int i = 0; i < emp_x.length; i++) {
+			empty_x.add(Double.parseDouble(emp_x[i]));
+			empty_y.add(Double.parseDouble(emp_y[i]));
+		}
 	}
 
 }
