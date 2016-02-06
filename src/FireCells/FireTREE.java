e5849c1032121212324c1281d49680fabfcc4005
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-06 11:12:26 -0500

    Added comments to FireTree class

--- src/FireCells/FireTREE.java
+++ src/FireCells/FireTREE.java
@@ -7,7 +7,10 @@ public class FireTREE extends FireCell {
 	private static final boolean isBurning = false;
 	private static final boolean isEmpty = false;
 	private static double probCatch;
-
+	
+	/*
+	 * Sets the that a FireTREE cell is empty and is burning
+	 */
 	public FireTREE(double x, double y, double w, double h, double probCatch) {
 		super(x, y, Color.GREEN, w, h, probCatch);
 		// TODO Auto-generated constructor stub
@@ -15,7 +18,12 @@ public class FireTREE extends FireCell {
 		setBurning(isBurning);
 		this.probCatch = probCatch;
 	}
-
+	
+	/*
+	 * Checks whether the neighbors of the FireTREE cell include a burning tree, if so, it gets a random number
+	 * and checks if its less than the probability of catching fire. If it is smaller than the probability of catching fire,
+	 * then it sets the cell to a new burning cell, otherwise it just returns itself.
+	 */
 	@Override
 	public FireCell update(FireCell[] cells) {
 		// TODO Auto-generated method stub
@@ -25,6 +33,9 @@ public class FireTREE extends FireCell {
 				? newFireCell : this;
 	}
 	
+	/*
+	 * Checks for if any of a cell's neighbors are burning.
+	 */
 	private boolean hasBurningNeighbor(FireCell[] cells) {
 		for (int i : getNeighbor()) {
 			if (cells[i].isBurning()) {
