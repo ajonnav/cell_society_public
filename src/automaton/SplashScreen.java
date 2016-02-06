8e0930447fef2a1cf233d5f9536ecd738ffc6e28
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-06 00:34:22 -0500

    Updated SplashScreen class

--- src/automaton/SplashScreen.java
+++ src/automaton/SplashScreen.java
@@ -73,7 +73,6 @@ public class SplashScreen {
 	private void openAutomationWindow(Map<String, String> argsMap) {
 		AutomatonDisplay myAutomation = new AutomatonDisplay(argsMap);
 		myAutomation.loadAutomaton();
-
 	}
 	
 	/**
