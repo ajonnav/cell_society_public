550fa59f063ab8e86983a559cce32f9181f51999
Colin Duffy <theduffman96@gmail.com>
2016-02-11 16:44:37 -0500

    Refactored SegregationTypeOne and Two to get rid of duplicated code

--- src/cells/TypeOneSegregationSquareCell.java
+++ src/cells/TypeOneSegregationSquareCell.java
@@ -4,25 +4,22 @@ import javafx.scene.paint.Color;
 
 public class TypeOneSegregationSquareCell extends SegregationTypeSquareCell {
 	private double tPercentage;
-	private static final int STATE = 1;
-	private static final int OTHER_STATE = 2;
 	private static final Color COLOR = Color.BLUE;
 	public TypeOneSegregationSquareCell(double x, double y, double w, double h,
 			double t) {
-		super(x, y, COLOR, w, h, t);
-		state = STATE;
+		super(x, y, COLOR, w, h, t, 1);
+		
 
 	}
 
 	public TypeOneSegregationSquareCell(SegregationSquareCell v, double t) {
-		super(v.getX(), v.getY(), COLOR, v.getWidth(), v.getHeight(), t);
-		state = STATE;
+		super(v, t, 1);
 	}
 	/**
 	 * Returns true if the number of similar/total neighbors ratio is less than the tPercentage
 	 */
 	public boolean cellUnsatisfied(SegregationSquareCell[] cells) {
-		double numSimilar = getNumOfState(getNeighbor(), state, cells);
+		double numSimilar = getNumOfState(getNeighbor(), STATE, cells);
 		double numNotSimilar = getNumOfState(getNeighbor(), OTHER_STATE, cells);
 		return ((numSimilar / (numNotSimilar + numSimilar)) < tPercentage);
 	}
