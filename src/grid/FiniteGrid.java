1a4b52fba9b40655fefdefa84e5cc9ec70880674
Colin Duffy <theduffman96@gmail.com>
2016-02-11 22:34:09 -0500

    FiniteGrid in theory should be working.  setNeighbors can work with any given number of directions

--- src/grid/FiniteGrid.java
+++ src/grid/FiniteGrid.java
@@ -2,7 +2,10 @@ package grid;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import slot.*;
 
 
@@ -12,6 +15,7 @@ public class FiniteGrid implements AnyGrid {
 	//private final Slot SLOT_TYPE;
 	SquareSlot sq;
 	Direction[] directions;
+	Map<String, Slot> rcMap;
 	public FiniteGrid(int r, int c, int slotType, int length, Direction [] neighborsToCheck) {
 		// TODO Auto-generated constructor stub
 		rows = r;
@@ -28,13 +32,24 @@ public class FiniteGrid implements AnyGrid {
 	public void initializeGrid() {
 		// TODO Auto-generated method stub
 		int index = 0;
+		rcMap = new HashMap<String,Slot>();
 		for(int i = 0; i < rows; i++){
 			for (int j = 0; j < cols; j++){
-				slotList.add(new SquareSlot(i, j, slotLength, slotLength, index)); // will change when I figure out how to choose which slot type to build
+				Slot s = new SquareSlot(i, j, slotLength, slotLength, index);
+				slotList.add(s); // will change when I figure out how to choose which slot type to build
+				String ij = buildKey(i, j);
+				rcMap.put(ij, s);
 				index++;
 			}
 		}
 		setNeighbors();
+	}
+	private String buildKey(int i, int j) {
+		StringBuilder st = new StringBuilder();
+		st.append(Integer.toString(i));
+		st.append(Integer.toString(j));
+		String ij=  st.toString();
+		return ij;
 	}
 	
 	private void setNeighbors(){
@@ -43,8 +58,15 @@ public class FiniteGrid implements AnyGrid {
 				int[] rcDelta = new int[2];
 				rcDelta[0] = s.getRowCol()[0] + d.getVec()[0];
 				rcDelta[1] = s.getRowCol()[1] + d.getVec()[1];
+				if(inBounds(rcDelta)){
+					s.addNeighbor(rcMap.get(buildKey(rcDelta[0], rcDelta[1])));
+				}
 			}
 		}
+	}
+	
+	private boolean inBounds(int[] rcDelta){
+		return (0 <= rcDelta[0] && rcDelta[0] < rows && 0 <= rcDelta[1] && rcDelta[1] < cols && rcDelta[0] != rcDelta[1]);
 	}
 
 	@Override
