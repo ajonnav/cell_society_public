69fe91d415c8ed04920c129e16ac972bca34d019
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-12 18:02:16 -0500

    Made a folder and a class to help with graphing cell populations.

---
+++ src/graphing/GraphData.java
@@ -0,0 +1,17 @@
+package graphing;
+
+import cells.Cell;
+
+public class GraphData {
+	Cell[] allCells;
+	
+	public GraphData() {
+	
+	}
+	
+	public GraphData(Cell[] allCells)  {
+		this.allCells = allCells;
+	}
+	
+
+}
