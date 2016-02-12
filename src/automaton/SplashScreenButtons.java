b255ebf0e2d1459954cb81996fc91fd7c48c3f43
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-12 15:32:10 -0500

    Modified splash screen buttons.

--- src/automaton/SplashScreenButtons.java
+++ src/automaton/SplashScreenButtons.java
@@ -1,12 +1,28 @@
 package automaton;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+
 import javafx.scene.Group;
+import javafx.scene.control.Control;
+import javafx.scene.layout.HBox;
 
 public class SplashScreenButtons extends AutomatonButtons {
-
-	public SplashScreenButtons(Group root, String language) {
+	private static final String DEFAULT_RESOURCE_PACKAGE = "ResourceBundle/";
+	private static final int HBOX_SPACING = 10;
+	private HBox hbox;
+	private Group root;
+	private Collection<Control> myControllers;
+	private AutomatonControlFactory a;
+	
+	public SplashScreenButtons(Group root, String language) throws IOException {
 		super(root, language);
-		// TODO Auto-generated constructor stub
+		
+		this.root = root;
+		XMLArgs x = new XMLArgs();
+		hbox = new HBox(HBOX_SPACING);
+		HashMap<String, String> myDefaults = x.readXML("testdefaultSplash.xml");
 	}
 
 }
