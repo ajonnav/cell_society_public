e270c8ebd21cba27ec28c993d739433cdeb04786
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-13 16:13:23 -0500

    Added method for giving back direction vectors based on the cardinal direction.

--- src/ForgingAntCells/AntOrientationFactory.java
+++ src/ForgingAntCells/AntOrientationFactory.java
@@ -1,9 +1,50 @@
 package ForgingAntCells;
 
 public class AntOrientationFactory {
-
 	public AntOrientationFactory() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	private void findForwardNeighbors(CardinalDirection d) {
+		int[] dx; 
+		int[] dy;
+		switch (d) {
+			case N:
+				dx = new int[]{-1, -1, -1};
+				dy = new int[]{-1, 0, 1};
+				break;
+			case NE: 
+				dx = new int[]{-1, -1, 0};
+				dy = new int[]{0, 1, 1};
+				break;
+			case E:
+				dx = new int[]{-1, 0, 1};
+				dy = new int[]{-1, 1, 1};
+				break;
+			case SE: 
+				dx = new int[]{0, 1, 1};
+				dy = new int[]{1, 1, 0};
+				break;
+			case S:
+				dx = new int[]{1, 1, 1};
+				dy = new int[]{1, 0, 1};
+				break;
+			case SW:
+				dx = new int[]{1, 1, 0};
+				dy = new int[]{0, -1, -1};
+				break;
+			case W:
+				dx = new int[]{1, 0, -1};
+				dy = new int[]{-1, -1, -1};
+				break;
+			case NW:
+				dx = new int[]{0, -1, -1};
+				dy = new int[]{-1, -1, 0};
+				break;
+			default:
+				break;
+		}
+		// give these to some other method to calculate slot numbers and return a list of slots
 	}
 
 }
