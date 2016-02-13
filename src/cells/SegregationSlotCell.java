bfed91bec1b9b3693a9c146272a4e62fa8beba2e
Colin Duffy <theduffman96@gmail.com>
2016-02-13 14:17:12 -0500

    Segregation Still getting redone

--- src/cells/SegregationSlotCell.java
+++ src/cells/SegregationSlotCell.java
@@ -1,19 +1,34 @@
 package cells;
 
+import java.util.ArrayList;
+
 import javafx.scene.paint.Color;
 
 public class SegregationSlotCell extends SegregationTypeSquareCell {
 
 	public SegregationSlotCell( Color c,
-			double t, int s) {
+			double t, int s, int o) {
 		super(0.0, 0.0, c, 0.0, 0.0, t);
 		setState(s);
+		OTHER_STATE = o;
 		// TODO Auto-generated constructor stub
 	}
 
 	public SegregationSlotCell(SegregationSquareCell v, double t, int type) {
 		super(v, t, type);
 		// TODO Auto-generated constructor stub
+	}
+	
+	//public boolean cellUnsatisfied(ListSegregationSlotCell)
+	
+	private double getNumOfState(ArrayList<Integer> neighborStates, int stateToCheck) {
+		double count = 0.0;
+		for (int i : neighborStates) {
+			if (i == stateToCheck)
+				count++;
+		}
+
+		return count;
 	}
 
 }
--- src/simulations/SegregationGrid.java
+++ src/simulations/SegregationGrid.java
@@ -8,38 +8,44 @@ import slot.Slot;
 import automaton.AutomatonDisplay;
 import automaton.XMLArgs;
 import grid.*;
+
 public class SegregationGrid extends CA {
 	private double tPercentage;
 	private double typeOneP;
 	private double vacantP;
 	private AnyGrid simGrid;
+
 	public SegregationGrid(XMLArgs xmlArgs, AutomatonDisplay a) {
 		super(xmlArgs, a);
 		// TODO Auto-generated constructor stub
 		tPercentage = xmlArgs.getAsDouble("tPercentage");
 		typeOneP = xmlArgs.getAsDouble("typeOneP");
-		vacantP = xmlArgs.getAsDouble("vacantP"); 
-		simGrid = new FiniteGrid(getNumRow(), getNumCol(), 1, (int)getCellWidth(), Direction.ALL_DIRECTIONS);
+		vacantP = xmlArgs.getAsDouble("vacantP");
+		simGrid = new FiniteGrid(getNumRow(), getNumCol(), 1,
+				(int) getCellWidth(), Direction.ALL_DIRECTIONS);
 	}
 
 	@Override
 	public void initializeScreen() {
 		// TODO Auto-generated method stub
 		simGrid.initializeGrid();
-		for(Slot s : simGrid.getSlots()){
+		for (Slot s : simGrid.getSlots()) {
 			double typeOne = (1.0 - vacantP) * typeOneP;
 			double rand = Math.random();
 			if (rand < vacantP) {
-				SegregationSlotCell cell = new SegregationSlotCell(Color.GRAY, tPercentage, 0);
+				SegregationSlotCell cell = new SegregationSlotCell(Color.GRAY,
+						tPercentage, 0,0);
 				s.addOccupant(cell);
 			} else if (rand > vacantP && rand < vacantP + typeOne) {
-				SegregationSlotCell cell = new SegregationSlotCell(Color.RED, tPercentage, 1);
+				SegregationSlotCell cell = new SegregationSlotCell(Color.RED,
+						tPercentage, 1,2);
 				s.addOccupant(cell);
 			} else {
-				SegregationSlotCell cell = new SegregationSlotCell(Color.BLUE, tPercentage, 2);
+				SegregationSlotCell cell = new SegregationSlotCell(Color.BLUE,
+						tPercentage, 2,1);
 				s.addOccupant(cell);
 			}
-			
+
 		}
 	}
 
@@ -52,12 +58,28 @@ public class SegregationGrid extends CA {
 	@Override
 	public void updateCells() {
 		// TODO Auto-generated method stub
-		for(Slot s : simGrid.getSlots()){
+		for (Slot s : simGrid.getSlots()) {
 			ArrayList<Cell> occupant = new ArrayList<Cell>(s.getOccupants());
-			SegregationSlotCell resident = (SegregationSlotCell)occupant.get(0);
-			if(resident.cellUnsatisfied(cells)
-					}
+			SegregationSlotCell resident = (SegregationSlotCell) occupant
+					.get(0);
+			ArrayList<Integer> neighborStates = getNeighborStates(s);
+			if(resident.isUnsatisfied(neighborStates)){
+				
+			}
+			
+		}
 
+	}
+
+	private ArrayList<Integer> getNeighborStates(Slot s) {
+		ArrayList<Integer> states = new ArrayList<Integer>();
+		ArrayList<Slot> neighbors = new ArrayList<Slot>(s.getNeighbors());
+		for (Slot n : neighbors) {
+			SegregationSlotCell neigh = (SegregationSlotCell) new ArrayList<Cell>(
+					n.getOccupants()).get(0);
+			states.add(neigh.getState());
+		}
+		return states;
 	}
 
 	@Override
