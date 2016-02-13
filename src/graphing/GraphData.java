fa6976e87b97fd03efc4916da6c5ce8e53ded82f
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-12 22:57:30 -0500

    Fix bug in graph data class.

--- src/graphing/GraphData.java
+++ src/graphing/GraphData.java
@@ -8,17 +8,22 @@ import cells.Cell;
 public class GraphData {
 	private Cell[] allCells;
 	private Map<String, Integer> cellData;
+	private int cycle;
 	
 	public GraphData() {
-	
+		setCycle(0);
 	}
 	
-	public GraphData(Cell[] allCells)  {
+	public void setGraphData(Cell[] allCells)  {
+		setCycle(getCycle() + 1);
 		this.allCells = allCells;
 		cellData = new HashMap<String, Integer>();
+		seperateData();
 	}
 	
-	
+	public Map<String, Integer> getMap() {
+		return cellData;
+	}
 	
 	private void seperateData() {
 		for (Cell c : allCells) {
@@ -29,6 +34,14 @@ public class GraphData {
 				cellData.put(key, cellData.get(key) + 1);
 			}
 		}
+	}
+
+	public int getCycle() {
+		return cycle;
+	}
+
+	public void setCycle(int cycle) {
+		this.cycle = cycle;
 	}
 
 }
