b3804df0253adae75b7dff2a326de84e53d79df5
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-13 21:53:50 -0500

    Made changes to the forging ant simulation.

--- src/simulations/ForgingAnts.java
+++ src/simulations/ForgingAnts.java
@@ -1,11 +1,24 @@
 package simulations;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
+import ForgingAntCells.AntCell;
+import ForgingAntCells.CardinalDirection;
+import ForgingAntCells.GroundCell;
 import automaton.AutomatonDisplay;
 import automaton.XMLArgs;
+import cells.Cell;
+import slot.Slot;
 
 public class ForgingAnts extends CA {
+	List<Slot> allSlots;
+	List<Integer> homeSpot;
+	List<Integer> foodSpot;
+	int tcol;
+	double addPheromone;
 
 	public ForgingAnts(XMLArgs xmlArgs, AutomatonDisplay a) {
 		super(xmlArgs, a);
@@ -28,6 +41,48 @@ public class ForgingAnts extends CA {
 	@Override
 	public void updateCells() {
 		//loop through slots
+		List<Slot> updatedSlots = new ArrayList<Slot>(allSlots);
+		List<GroundCell> groundCells = new ArrayList<GroundCell>();
+		double[] antDropFoodPheromone = new double[allSlots.size()];
+		double[] antDropHomePheromone = new double[allSlots.size()];
+		double[] diffusionFoodPheromone = new double[allSlots.size()];
+		double[] diffusionHomePheromone = new double[allSlots.size()];
+		for (Slot s : updatedSlots) {
+			Collection<Cell> occupants = s.getOccupants();
+			if (homeSpot.contains(s.index())) {
+				//write for ant cell
+			} else if (foodSpot.contains(s.index())) {
+				//write for ground cell
+			} else {
+				for (Cell c : occupants) {
+					if (c instanceof AntCell) {
+						if (((AntCell) c).getFoodStatus() == true) {
+							antDropHomePheromone[s.index()] += addPheromone;
+						} else {
+							antDropFoodPheromone[s.index()] += addPheromone;
+						}
+						Slot nextSlot = ((AntCell) c).findBestNeighbor(allSlots.get(s.index()));
+						if (nextSlot == null) {
+							nextSlot = s;
+						}
+						((AntCell) c).setDirection(getDirectionbetweenSlots(s, nextSlot));
+						updatedSlots.get(nextSlot.index()).getOccupants().add(c);
+						updatedSlots.get(s.index()).getOccupants().remove(c);
+						
+					} else {
+						groundCells.add((GroundCell) c);
+						double [] diffusion = ((GroundCell) c).getDiffusionAmount();
+						diffusionFoodPheromone[s.index()] += diffusion[0];
+						diffusionHomePheromone[s.index()] += diffusion[1];
+					}
+				}
+			}
+		}
+		for (GroundCell g : groundCells) {
+			g.setfoodHormone(antDropFoodPheromone[g.getSlot().index()] + diffusionFoodPheromone[g.getSlot().index()]);
+			g.sethomeHormone(antDropHomePheromone[g.getSlot().index()] + diffusionHomePheromone[g.getSlot().index()]);
+			g.update();
+		}
 			//get occupants in slot
 				//loop through slot
 					//if its ant check if its at home or at food source
@@ -47,6 +102,17 @@ public class ForgingAnts extends CA {
 	public void drawCells() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private CardinalDirection getDirectionbetweenSlots(Slot origin, Slot next) {
+		if (origin.index() == next.index()) {
+			return null;
+		}
+		int oldx = origin.index() / tcol;
+		int oldy = origin.index() % tcol;
+		int newx = next.index() / tcol;
+		int newy = next.index() % tcol;
+		return CardinalDirection.valueOf(newx - oldx, newy - oldy);
 	}
 
 }
