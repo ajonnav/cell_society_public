daa27258f25db614860924ad599e5357e440371d
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-17 01:58:59 -0500

    Changes for Masterpiece.

--- src/ForgingAntCells/AntOrientationFactory.java
+++ src/ForgingAntCells/AntOrientationFactory.java
@@ -9,44 +9,47 @@ import slot.Slot;
 /*
  * Author: Christine Zhou
  */
+//This entire file is part of my masterpiece.
+//Christine Zhou
+/*
+ * My ant factory takes the slot and the current direction of an ant and finds its forward and backwards neighbors. It has
+ * getters for the ant cell to retreive its forward and backward neighbors. 
+ * I included this factory class for determining orientation because this class separated out the very difficult details
+ * of getting directional neighbors as part of the ant simulation from the simulation class Forging Ants and Ant Cell class.
+ * Even though the code for getting the directional neighbors is still a very long switch case, by moving it out of the rest
+ * of the simulations and the cells, I made it easier in the future if I want to add directions or change how the new directional
+ * neighbors are determined. I think this is a good design choice because the implementation details of getting a list of
+ * forward or backward neighobrs of an ant given its current orientation is hidden from the rest of the forging ants code.
+ * This way, the brute force code for finding directional neighbors never has to appear in the ant class and I don't need 8 
+ * classes of an ant just because each finds a neighbor a different way based on its 8 orientations. I also assummed when
+ * I refactored that the slots class can now return a collection of neighbors if a list of directional arrays is passed in.
+ * I think this should have originally been implemented in our slots class because the math for getting neighbors from direction
+ * arrays was in our slot class and was being used to calculate neighbors. If I calculated neighbors with directional arrays
+ * again in the forging ants class or in the factory class, then I would be repeating the calculation code that should only
+ * have to appear once in the slots class.
+ */
 public class AntOrientationFactory {
 	private Collection<Slot> forwardNeighbors;
 	private Collection<Slot> backwardNeighbors;
-	private Collection<Slot> Neighbors;
+	private Collection<Slot> neighbors;
 	
-	private int tcol;
-	
-	public AntOrientationFactory() {	
-	}
-	
-	public AntOrientationFactory(CardinalDirection d, Slot s, int tcol) {
-		Neighbors = s.getNeighbors();
+	public AntOrientationFactory() {
 		forwardNeighbors = new ArrayList<Slot>();
 		backwardNeighbors = new ArrayList<Slot>();
-		this.tcol = tcol;
+	}
+	
+	public void findNeighbors(CardinalDirection d, Slot s) {
+		neighbors = s.getNeighbors();
 		findNeighborDirection(d, s);
 	}
 	
-	public Collection<Slot> getForwardNeighbors() {
-		return forwardNeighbors;
-	}
-	
-	public Collection<Slot> getBackwardNeighbors() {
-		return backwardNeighbors;
-	}
 	
 	private void getSlotsfromArray(Slot s, int[] dx, int[] dy) {
-		int index = s.index();
-		int myX = index / tcol;
-		int myY = index % tcol;
-		List<Integer> f_indexes = new ArrayList<Integer>();
-		for (int k = 0; k < dx.length; k++) {
-			f_indexes.add((myX + dx[k])*(tcol) + (myY + dy[k]));
-		}
-		for (Slot slot : Neighbors) {
-			if (f_indexes.contains(slot.index())) {
-				forwardNeighbors.add(slot);
-			} else {
+		//assumes that slot has can give back a list of neighbors with those direction vectors
+		//this is definitely possible since our slot neighbors were originally calculated by passing in a direction vector
+		forwardNeighbors = s.findNeighbors(dx, dy);
+		for (Slot slot : neighbors) {
+			if (!forwardNeighbors.contains(slot)) {
 				backwardNeighbors.add(slot);
 			}
 		}
@@ -100,5 +103,18 @@ public class AntOrientationFactory {
 		}
 		getSlotsfromArray(s, dx, dy);
 	}
-
+	
+	/*
+	 * Returns the forward neighbors, must be called after findNeighbors 
+	 */
+	public Collection<Slot> getForwardNeighbors() {
+		return forwardNeighbors;
+	}
+	
+	/*
+	 * Returns the backward neighbors, must be called after findNeighbors 
+	 */
+	public Collection<Slot> getBackwardNeighbors() {
+		return backwardNeighbors;
+	}
 }
